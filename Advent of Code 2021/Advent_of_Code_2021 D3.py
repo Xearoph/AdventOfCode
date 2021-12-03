7a2072b36e2844e57acd2b42a7bc3a5b27a6de0a
Xearoph <kvanbeek@rocvantwente.nl>
2021-12-03 21:16:07 +0100

    d3p1

--- Advent of Code 2021/Advent_of_Code_2021 D3.py
+++ Advent of Code 2021/Advent_of_Code_2021 D3.py
@@ -5,13 +5,30 @@ with open('inputD3.txt','r') as f:
 
 countCommon1 = 0;
 countCommon0 = 0;
+gamma = '';
+epsilon = '';
 
-for x in range(len(lines[x])) :
-	for y in range(len(lines)) :
-		if lines[y][x] == '1' :
+for y in range(len(lines[0])) :
+	countCommon0 = 0
+	countCommon1 = 0
+	for x in range(len(lines)) :
+		if lines[x][y] == '1' :
 			countCommon1 += 1
 		else :
 			countCommon0 += 1
+	if countCommon0 > countCommon1 :
+		gamma += '0'
+	else :
+		gamma += '1'
 
-print('1',countCommon1)
-print('0',countCommon0)
+for x in range(len(gamma)) :
+	if gamma[x] == '1' :
+		epsilon += '0'
+	else :
+		epsilon += '1'
+
+gammaConverterd = int(gamma,2)
+epsilonConverted = int(epsilon,2)
+print('gamma',gammaConverterd)
+print('epsilon',epsilonConverted)
+print(gammaConverterd *epsilonConverted)
