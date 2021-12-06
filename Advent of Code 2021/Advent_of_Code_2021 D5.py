4235a12a18d010db2e27d502837295679869aaa9
Xearoph <kvanbeek@rocvantwente.nl>
2021-12-07 00:40:18 +0100

    d6

--- Advent of Code 2021/Advent_of_Code_2021 D5.py
+++ Advent of Code 2021/Advent_of_Code_2021 D5.py
@@ -5,10 +5,11 @@ with open('inputD5.txt','r') as f:
 	lines = lines.replace("\n",",")
 	lines = lines.split(",")
 
-vector2start = np.zeros((int(len(lines)/4),2), dtype=int)
-vector2end = np.zeros((int(len(lines)/4),2), dtype=int)
+boardSize = 5000
+vector2start = np.zeros((boardSize,2), dtype=int)
+vector2end = np.zeros((boardSize,2), dtype=int)
 
-board = np.chararray((10,10),unicode=True)
+board = np.chararray((boardSize,boardSize),unicode=True)
 board[:] = '.'
 
 i = 0
@@ -24,16 +25,80 @@ for x in range(int(len(lines)/4)) :
 	i+=2
 	j+=2
 
-for x in range(int(len(lines)/4)) :
 
-	if board[vector2start[x,0],vector2start[x,1]] == '0' :
-		board[vector2start[x,0],vector2start[x,1]] = '1'
-	else :
-		board[vector2start[x,0],vector2start[x,1]] = '0'
+markFields = []
 
-	if board[vector2end[x,0],vector2end[x,1]] == '0' :
-		board[vector2end[x,0],vector2end[x,1]] = '1'
-	else :
-		board[vector2end[x,0],vector2end[x,1]] = '0'
+for x in range(len(vector2start)) :
+	firstX = vector2start[x][0]
+	firstY = vector2start[x][1]
 
+	lastX = vector2end[x][0]
+	lastY = vector2end[x][1]
+	
+	# als de eerste x waarde lager is dan de tweede en de y waardes zijn gelijk
+	if firstX < lastX and firstY == lastY :
+		lineCounter = lastX - firstX
+		for x in range(lineCounter+1) :
+			markFields.append([firstX+x,lastY])
+
+	# als de eerste x waarde groter is dan de tweede en de y waardes zijn gelijk
+	if firstX > lastX and firstY == lastY :
+		lineCounter = firstX - lastX 
+		for x in range(lineCounter+1) :
+			markFields.append([firstX-x,lastY])
+
+	# als de eerste x waarde kleiner is dan de tweede en de x waardes zijn gelijk
+	if firstX == lastX and firstY > lastY :
+		lineCounter = firstY - lastY
+		for x in range(lineCounter+1) :
+			markFields.append([firstX,lastY+x])
+	
+	# als de eerste x waarde kleiner is dan de tweede en de x waardes zijn gelijk
+	if firstX == lastX and firstY < lastY :
+		lineCounter =  lastY - firstY
+		for x in range(lineCounter+1) :
+			markFields.append([firstX,lastY-x])
+	
+	# als de eerste x waarde kleiner is dan de tweede en de x waardes zijn gelijk
+	if firstX < lastX and firstY < lastY :
+		lineCounter = lastY - firstY
+		for x in range(lineCounter+1) :
+			markFields.append([firstX+x,lastY-x])
+	
+	# als de eerste x waarde kleiner is dan de tweede en de x waardes zijn gelijk
+	if firstX > lastX and firstY > lastY :
+		lineCounter = firstY - lastY
+		for x in range(lineCounter+1) :
+			markFields.append([firstX-x,firstY-x])
+	
+	# als de eerste x waarde kleiner is dan de tweede en de x waardes zijn gelijk
+	if firstX > lastX and firstY < lastY :
+		lineCounter = lastY - lastX
+		for x in range(lineCounter+1) :
+			markFields.append([firstX-x,lastY-x])
+	
+	# als de eerste x waarde kleiner is dan de tweede en de x waardes zijn gelijk
+	if firstX < lastX and firstY > lastY :
+		lineCounter = firstX - lastY
+		for x in range(lineCounter+1) :
+			markFields.append([firstX+x,firstY-x])
+
+	
+
+
+twoos = 0
+for x in range(int(len(markFields))) :
+		spot = board[markFields[x][1],markFields[x][0]]
+		if spot == '.' :
+			board[markFields[x][1],markFields[x][0]] = '1'
+		elif spot == '1' :
+			board[markFields[x][1],markFields[x][0]] = '2'
+			twoos+=1
+		elif spot == '2' :
+			board[markFields[x][1],markFields[x][0]] = '3'
+		elif spot == '3' :
+			board[markFields[x][1],markFields[x][0]] = '4'
+		elif spot == '4' :
+			board[markFields[x][1],markFields[x][0]] = '5'
 print(board)
+print(twoos)
---
+++ Advent of Code 2021/Advent_of_Code_2021 D6.py
@@ -0,0 +1,22 @@
+with open('inputD6.txt','r') as f:
+	lines = f.read().split(",")
+
+lines = list(map(int, lines))
+days = 256
+count = 0
+for y in range(days) :
+	for x in range(len(lines)) :
+		if lines[x] == 0 :
+			lines[x] = 6
+			lines.append(8)
+		else :
+			lines[x] -= 1
+
+global totalFish
+totalFish = 0
+
+for x in range(len(lines)) :
+	totalFish += 1
+
+print(totalFish)
+
